80b03ecd4ca57a6eac773f666252f08326cd99db
Eugene Mirotin <emirotin@gmail.com>
2011-01-18 23:48:59 +0200

    refactoring for supporting comments

--- WpPygments.php
+++ WpPygments.php
@@ -10,72 +10,25 @@ Description: Colorizes code in WP posts with Pygments.
 $SERVICE_URL = 'http://pygmentizer.appspot.com/';
 
 require_once('php/phpQuery.php');
-
-// based on http://wezfurlong.org/blog/2006/nov/http-post-from-php-without-curl
-// and http://nadeausoftware.com/articles/2007/07/php_tip_how_get_web_page_using_fopen_wrappers
-function http_request($url, $params = null, $verb = 'GET')
-{
-  $cparams = array(
-    'http' => array(
-      'method' => $verb,
-      'ignore_errors' => true
-    )
-  );
-  if ($params !== null) {
-    $params = http_build_query($params);
-    if ($verb == 'POST') {
-      $cparams['http']['content'] = $params;
-    } else {
-      $url .= '?' . $params;
-    }
-  }
-  
-  $result = array();
-
-  $context = stream_context_create($cparams);
-  $fp = fopen($url, 'rb', false, $context);
-  if (!$fp) {
-    return null;
-  } else {
-    $page = stream_get_contents($fp);
-  }
-  
-  //$page    = @file_get_contents( $url, false, $context );
-
-  if ( $page != false )
-    $result['content'] = $page;
-  else if ( !isset( $http_response_header ) )
-    return null;    // Bad url, timeout
-
-  // Save the header
-  $result['header'] = $http_response_header;
-
-  // Get the *last* HTTP status code
-  $nLines = count( $http_response_header );
-  for ( $i = $nLines-1; $i >= 0; $i-- )
-  {
-    $line = $http_response_header[$i];
-    if ( strncasecmp( "HTTP", $line, 4 ) == 0 )
-    {
-      $response = explode( ' ', $line );
-      $result['status'] = $response[1];
-      break;
-    }
-  }
- 
-  return $result;
-}
+require_once('php/http_request.php');
 
 function _callPygmentizeService($url, $code, $lang = '') {
-  return http_request($url, array('lang' => $lang, 'code' => $code), 'POST');
+  $res = http_request($url, array('lang' => $lang, 'code' => $code), 'POST');
+  if ($res !== null && $res['status'] == '200')
+    return $res['content'];
+  return false;
 }
 
 
 if (!class_exists("WpPygments")) {
   class WpPygments {
     function WpPygments() { //constructor
-      
+      $this->styles = array('autumn', 'borland', 'bw', 'colorful', 'default', 
+                            'emacs', 'friendly', 'fruity', 'manni', 'monokai', 
+                            'murphy', 'native', 'pastie', 'perldoc', 'tango',
+                            'trac', 'vim', 'vs');
     }
+    
     function init() {
       wp_enqueue_script('jquery');
       wp_enqueue_script('jquery-ui-dialog');
@@ -95,8 +48,8 @@ if (!class_exists("WpPygments")) {
       echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . 
         '/wp-content/plugins/WpPygments/css/pygments/' . $style_name . '.css" />' . "\n";
     }
-        
-    static function _processCodeNode($el) {
+
+    static function _getPygmentizedCode($el) {
       global $SERVICE_URL;
       $el = pq($el);
       $lang = $el->attr('lang');
@@ -105,8 +58,15 @@ if (!class_exists("WpPygments")) {
       
       $code = $el->text();
       $res = _callPygmentizeService($SERVICE_URL, $code, $lang);
-      if ($res !== null && $res['status'] == '200') {  
-        $el->parent()->wrap('<div class="highlight-wrapper ' . $lang . '"</div>');
+      return array('res' => $res, 'lang' => $lang);      
+    }
+
+    static function _processCodeNode($el) {
+      $res = self::_getPygmentizedCode($el);
+      
+      if ($res['res'] !== false) {
+        $el = pq($el);
+        $el->parent()->wrap('<div class="highlight-wrapper ' . $res['lang'] . '"></div>');
         $wrap = $el->parent()->parent();
         $el->parent()->addClass('raw');
         $wrap->prepend('<div class="tools">' .
@@ -116,22 +76,50 @@ if (!class_exists("WpPygments")) {
           '<a href="#" class="print">print</a>'.
           '<a href="#" class="about">?</a>'.
         '</div>');
-        $wrap->append('<div class="highlighted">' . $res['content'] . '</div>');
+        $wrap->append('<div class="highlighted">' . $res['res'] . '</div>');
       }
     }
-    
-    function processContent($content = '') {
-      $dialog = '<div id="about-dialog">' .
-        '<p>WpPygments is a Wordpress plugin that colorizes your code snippets using the web-service created around a popular Pygments library.</p>' .
-        '<p>Both the service and the plugin are developed by Eugene Mirotin. See <a href="http://google.com" target="_blank">here</a> how to add it to your blog.</p>' .
-      '</div>';
-      
+
+    function _processContent($content = '', $alter_content = true, $add_dialog = true) {
       $doc = phpQuery::newDocumentHTML($content);
+
+      if (!$alter_content) {
+        $doc->find('pre>code:not(.nocolor)')->each(array('WpPygments', '_getPygmentizedCode'));
+        return $content;
+      }
+
       $doc->find('pre>code:not(.nocolor)')->each(array('WpPygments', '_processCodeNode'));
       // marking line number as literals for coloring
-      $doc->find('.linenodiv pre')->addClass('nl'); 
-      return $dialog . $doc->htmlOuter();
+      $doc->find('.linenodiv pre')->addClass('nl');
+      $new_content = $doc->htmlOuter();
+      
+      if ($add_dialog) {
+        $dialog = '<div id="wppygments-about-dialog">' .
+          '<p>WpPygments is a Wordpress plugin that colorizes your code snippets using the web-service created around a popular Pygments library.</p>' .
+          '<p>Both the service and the plugin are developed by Eugene Mirotin. See <a href="http://google.com" target="_blank">here</a> how to add it to your blog.</p>' .
+        '</div>';
+        $new_content = $dialog . $new_content;
+      }
+      
+      return $new_content;      
     }
+    
+    function preShowContent($content = '') {
+      return $this->_processContent($content, true, true);
+    }
+
+    function preShowComment($comment = '') {
+      return $this->_processContent($comment, true, false);
+    }
+
+    function preSaveContent($content = '') {
+      return $this->_processContent($content, false);
+    }
+
+    function preSaveComment($comment = '') {
+      return $this->_processContent($content, false);
+    }
+
   } //End Class WpPygments
 }
 
@@ -144,8 +132,14 @@ if (isset($wp_pygments)) {
   //Actions
   add_action('wp_head', array(&$wp_pygments, 'processHeader'), 1);
   add_action('init',  array(&$wp_pygments, 'init'));
+  
   //Filters
-  add_filter('the_content', array(&$wp_pygments, 'processContent'),1); 
+  //add_filter('content_filtered_save_pre', array(&$wp_pygments, 'preSaveContent'), 1); 
+  //add_filter('pre_comment_content', array(&$wp_pygments, 'preSaveComment'), 1); 
+    
+  add_filter('the_content', array(&$wp_pygments, 'preShowContent'), 1); 
+  //add_filter('comment_text', array(&$wp_pygments, 'preShowComment'), 1);
+     
 }
 
 ?>
---
+++ php/http_request.php
@@ -0,0 +1,58 @@
+<?php
+/* 
+ * Compiled by Eugene Mirotin [http://blog.mirotin.net]
+ * based on http://wezfurlong.org/blog/2006/nov/http-post-from-php-without-curl
+ * and http://nadeausoftware.com/articles/2007/07/php_tip_how_get_web_page_using_fopen_wrappers
+ */
+
+function http_request($url, $params = null, $verb = 'GET')
+{
+  $cparams = array(
+    'http' => array(
+      'method' => $verb,
+      'ignore_errors' => true
+    )
+  );
+  if ($params !== null) {
+    $params = http_build_query($params);
+    if ($verb == 'POST') {
+      $cparams['http']['content'] = $params;
+    } else {
+      $url .= '?' . $params;
+    }
+  }
+  
+  $result = array();
+
+  $context = stream_context_create($cparams);
+  $fp = fopen($url, 'rb', false, $context);
+  if (!$fp) {
+    return null;
+  } else {
+    $page = stream_get_contents($fp);
+  }
+  
+  if ( $page != false )
+    $result['content'] = $page;
+  else if ( !isset( $http_response_header ) )
+    return null;    // Bad url, timeout
+
+  // Save the header
+  $result['header'] = $http_response_header;
+
+  // Get the *last* HTTP status code
+  $nLines = count( $http_response_header );
+  for ( $i = $nLines-1; $i >= 0; $i-- )
+  {
+    $line = $http_response_header[$i];
+    if ( strncasecmp( "HTTP", $line, 4 ) == 0 )
+    {
+      $response = explode( ' ', $line );
+      $result['status'] = $response[1];
+      break;
+    }
+  }
+ 
+  return $result;
+}
+?>
