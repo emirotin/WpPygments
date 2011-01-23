546d8c41151c4a629f2a1f7d6406201797e9b143
Eugene Mirotin <emirotin@gmail.com>
2011-01-23 19:49:48 +0200

    options page

--- WpPygments.php
+++ WpPygments.php
@@ -50,21 +50,35 @@ function WpPygments_install_db() {
     update_option("WpPygments_db_version", $WpPygments_db_version);
   }
 }
-
+                              
 if (!class_exists("WpPygments")) {
   class WpPygments {
     function WpPygments() { //constructor
       $this->styles = array('autumn', 'borland', 'bw', 'colorful', 'default', 
-                            'emacs', 'friendly', 'fruity', 'manni', 'monokai', 
-                            'murphy', 'native', 'pastie', 'perldoc', 'tango',
-                            'trac', 'vim', 'vs');
+                                  'emacs', 'friendly', 'fruity', 'manni', 'monokai', 
+                                  'murphy', 'native', 'pastie', 'perldoc', 'tango',
+                                  'trac', 'vim', 'vs');
+                                  
+      $this->cache_ttl = array(
+                               'never clean' => null,
+                               '6 months'    => '6 MONTH',
+                               '3 months'    => '2 MONTH',
+                               '30 days'     => '30 DAY',
+                               '15 days'     => '15 DAY',
+                               '10 days'     => '10 DAY',
+                               '5 days'      => '5 DAY',
+                               '2 days'      => '2 DAY',
+                               '1 day'       => '1 DAY'
+                              );
     }
     
     function init() {
       wp_enqueue_script('jquery');
       wp_enqueue_script('jquery-ui-dialog');
-      wp_enqueue_script('wppygments.zeroclipboard', get_bloginfo('wpurl') . '/wp-content/plugins/WpPygments/js/ZeroClipboard.js', array(), '1.0.7');
-      wp_enqueue_script('wppygments.printarea', get_bloginfo('wpurl') . '/wp-content/plugins/WpPygments/js/jquery.PrintArea.js', array('jquery'), '1.0.7');
+      wp_enqueue_script('wppygments.zeroclipboard', get_bloginfo('wpurl') . '/wp-content/plugins/WpPygments/js/ZeroClipboard.js', 
+        array(), '1.0.7');
+      wp_enqueue_script('wppygments.printarea', get_bloginfo('wpurl') . '/wp-content/plugins/WpPygments/js/jquery.PrintArea.js', 
+        array('jquery'), '1.0.7');
       wp_enqueue_script('wppygments.tools', get_bloginfo('wpurl') . '/wp-content/plugins/WpPygments/js/pygments-tools.js.php', 
         array('jquery', 'wppygments.zeroclipboard', 'wppygments.printarea'), '0.4');
     }
@@ -194,7 +208,9 @@ if (!class_exists("WpPygments")) {
         return $content;
     }
 
-    // clean obsolete cache records
+    /*
+     *  clean obsolete cache records
+     */
     function clean_old_cache() {      
       $ttl = 60; $unit = 'SECOND'; // TODO: read from settings
       $clear_old = true;
@@ -214,6 +230,96 @@ if (!class_exists("WpPygments")) {
     function unregister_cron() {
       wp_clear_scheduled_hook('clean_old_cache');
     }
+    
+    /*
+     * Options
+     */
+    function add_options_page() {
+      add_options_page('WpPygments Options', 'WpPygments', 'manage_options', 'WpPygments', array($this, 'create_options_page'));
+    }
+
+    function create_options_page() {
+    ?>
+    <div class="wrap">
+    <h2>WpPygments Settings</h2>
+    <form action="options.php" method="post">
+    <?php settings_fields('WpPygments_options'); ?>
+    <?php do_settings_sections('WpPygments'); ?>
+    <p class="submit">
+      <input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+    </p>
+    </form>
+    </div>
+    <?php
+    }
+    
+    function validate_options($input) {
+      $options = get_option('WpPygments_options');
+      
+      $style = $input['pygments_style'];
+      if (!in_array($style, $this->styles))
+        $style = 'default';
+      $options['pygments_style'] = $style;
+      
+      $options['pygments_toolbar_show_raw'] = $input['pygments_toolbar_show_raw'] === 'on' ? 'on' : 'off';
+      $options['pygments_toolbar_show_copy'] = $input['pygments_toolbar_show_copy'] === 'on' ? 'on' : 'off';
+      $options['pygments_toolbar_show_print'] = $input['pygments_toolbar_show_print'] === 'on' ? 'on' : 'off';
+      return $options;
+    }
+   
+    
+    function admin_init() {
+      register_setting('WpPygments_options', 'WpPygments_options', array($this, 'validate_options'));
+
+      add_settings_section('WpPygments_style', 'Style', array($this, 'options_style_section_desc'), 'WpPygments');
+      add_settings_field('pygments_style', 'Style', array($this, 'style_option'), 'WpPygments', 'WpPygments_style');
+
+      add_settings_section('WpPygments_toolbar', 'Toolbar', array($this, 'options_toolbar_section_desc'), 'WpPygments');
+      add_settings_field('pygments_toolbar_show_raw', 'Show Raw / Highlighted Switcher', array($this, 'toolbar_raw_option'), 'WpPygments', 'WpPygments_toolbar');
+      add_settings_field('pygments_toolbar_show_copy', 'Show Copy Link', array($this, 'toolbar_copy_option'), 'WpPygments', 'WpPygments_toolbar');
+      add_settings_field('pygments_toolbar_show_print', 'Show Print Link', array($this, 'toolbar_print_option'), 'WpPygments', 'WpPygments_toolbar');
+    }
+    
+    function options_style_section_desc() {
+      echo '<p>Choose on of the styles provided by the <a href="http://pygments.org">Pygments</a> library.';
+    }
+    function style_option() {
+      $options = get_option('WpPygments_options');
+      echo "<select id='pygments_style' name='WpPygments_options[pygments_style]'>";
+      foreach ($this->styles as $style) {
+        echo "<option value='{$style}'";
+        if ($style == $options['pygments_style'])
+          echo " selected='selected'";
+        echo ">{$style}</option>"; 
+      }
+      echo "</select>";
+    }
+
+    function options_toolbar_section_desc() {
+      echo '<p>Configure various toolbar links.';
+    }
+    function toolbar_raw_option() {
+      $options = get_option('WpPygments_options');
+      echo "<input id='pygments_toolbar_raw' name='WpPygments_options[pygments_toolbar_show_raw]' type='checkbox'";
+      if (!isset($options['pygments_toolbar_show_raw']) || $options['pygments_toolbar_show_raw'] === 'on')
+        echo " checked='checked'";
+      echo " />";
+    }
+    function toolbar_copy_option() {
+      $options = get_option('WpPygments_options');
+      echo "<input id='pygments_toolbar_copy' name='WpPygments_options[pygments_toolbar_show_copy]' type='checkbox'";
+      if (!isset($options['pygments_toolbar_show_copy']) || $options['pygments_toolbar_show_copy'] === 'on')
+        echo " checked='checked'";
+      echo " />";
+    }
+    function toolbar_print_option() {
+      $options = get_option('WpPygments_options');
+      echo "<input id='pygments_toolbar_print' name='WpPygments_options[pygments_toolbar_show_print]' type='checkbox'";
+      if (!isset($options['pygments_toolbar_show_print']) || $options['pygments_toolbar_show_print'] === 'on')
+        echo " checked='checked'";
+      echo " />";
+    }
+
 
   } //End Class WpPygments
 }
@@ -227,12 +333,14 @@ if (isset($wp_pygments)) {
   // Hooks
   register_activation_hook(__FILE__,'WpPygments_install_db');
   register_activation_hook(__FILE__, array(&$wp_pygments, 'register_cron'));
-  add_action('clean_old_cache', array(&$wp_pygments, 'clean_old_cache'));
   register_deactivation_hook(__FILE__, array(&$wp_pygments, 'unregister_cron')); 
   
   //Actions
   add_action('wp_head', array(&$wp_pygments, 'processHeader'), 1);
   add_action('init',  array(&$wp_pygments, 'init'));
+  add_action('admin_menu', array(&$wp_pygments, 'add_options_page'));
+  add_action('admin_init', array(&$wp_pygments, 'admin_init'));
+  add_action('clean_old_cache', array(&$wp_pygments, 'clean_old_cache'));
   
   //Filters
   add_filter('content_save_pre', array(&$wp_pygments, 'preSaveContent'), 1); 
