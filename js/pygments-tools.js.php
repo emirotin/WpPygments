7f399ce043134611d1eb9b4aa44f1de35da03ed8
Eugene Mirotin <emirotin@gmail.com>
2011-05-29 02:26:22 +0300

    links fixes

--- js/pygments-tools.js.php
+++ js/pygments-tools.js.php
@@ -15,7 +15,7 @@ var ZeroClipboard, jQuery;
 	  $('.highlighted').css('display', 'block');
 		$('.tools .show-raw, .tools .show-colored').click(function(){
 			var el = $(this),
-			    tools = el.parent(),
+			    tools = el.parent().parent(),
 			    wrap = tools.parent();
 			wrap.children('.highlighted').slideToggle();
       wrap.children('.raw').slideToggle();
@@ -49,7 +49,7 @@ var ZeroClipboard, jQuery;
 		// print
     $('.print').click(function(){
       var el = $(this),
-          code = el.parent().parent().find('.highlight');
+          code = el.parent().parent().parent().find('.highlight');
 			code.printArea();
 			return false;
     });
@@ -60,7 +60,7 @@ var ZeroClipboard, jQuery;
 		  if (!dialog.size()) {
         dialog = $('<div id="wppygments-about-dialog">' +
             '<p>WpPygments is a Wordpress plugin that colorizes your code snippets using the web-service created around a popular Pygments library.</p>' +
-            '<p>Both the service and the plugin are developed by Eugene Mirotin. See <a href="http://google.com" target="_blank">here</a> how to add it to your blog.</p>' +
+            '<p>Both the service and the plugin are developed by Eugene Mirotin.</p>' +
           '</div>');
         dialog.dialog({ autoOpen: false, title: 'WpPygments by Eugene Mirotin' });
       }
