7b4f9f26d0d64ec97e5cbfc9147a3ebd35fb7e82
emirotin <emirotin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-14 19:44:27 +0000

    Version 0.1 - an almost working plugin (currently points at my local service)
    TODO:
    - settings
    - convert post on save, not on display
    
    git-svn-id: http://svn.wp-plugins.org/wppygments/trunk@397130 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tools/gen_css.sh
@@ -0,0 +1,12 @@
+CSS_DIR="css/pygments"
+WRAPPER=".highlighted"
+
+cd "$(dirname "$0")/.."
+pwd
+
+for theme in 'monokai' 'manni' 'perldoc' 'borland' 'colorful' 'default' 'murphy' 'vs' 'trac' 'tango' 'fruity' 'autumn' 'bw' 'emacs' 'vim' 'pastie' 'friendly' 'native'; do
+/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin/pygmentize -S $theme -a $WRAPPER -f html > $CSS_DIR/$theme.css;
+java -jar tools/yuicompressor.jar -o $CSS_DIR/$theme.css $CSS_DIR/$theme.css
+done;
+
+
